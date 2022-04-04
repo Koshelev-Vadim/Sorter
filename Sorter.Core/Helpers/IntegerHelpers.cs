3eed5d577943628cb48e0ba6f6184ed2484d0330
Koshelev-Vadim <bone-duke@yandex.ru>
2022-04-04 22:59:04 +0300

    remove garbage

--- Sorter.Core/Helpers/IntegerHelpers.cs
+++ Sorter.Core/Helpers/IntegerHelpers.cs
@@ -13,27 +13,5 @@ namespace Sorter.Core.Helpers
             return res;
         }
 
-        public static void CopyToBytesArray(int num, byte[] data, ref int dataPosFrom)
-        {
-
-            //Encoding.ASCII.GetBytes()
-
-            123.ToString();
-            bool numIsStarted = false;
-            int positionValue;
-            int partNumb = 0;
-            for (var i = 9; i >= 0; i--)
-            {
-                positionValue = (int)((num - partNumb) / (Math.Pow(10, i)));
-                if (positionValue > 0)
-                    numIsStarted = true;
-
-                if (numIsStarted)
-                {
-                    data[dataPosFrom++] = (byte)(positionValue + 48); // '0'
-                    partNumb += (int)Math.Pow(10, i) * positionValue;
-                }
-            }
-        }
     }
 }
